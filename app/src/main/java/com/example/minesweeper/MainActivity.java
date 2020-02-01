1fd081d5bb1ed8602c9a2fea8831ff355b7a3e0c
hqnguyen <hqnguyen@sfu.ca>
2020-01-31 23:06:07 -0800

    Init commit: Create Grid

---
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.minesweeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int NUM_ROWS = 4;
+    private static final int NUM_COLS = 6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        populateButtons();
+    }
+
+    private void populateButtons() {
+        TableLayout table = (TableLayout) findViewById(R.id.tableForButtons);
+
+        for(int row = 0; row < NUM_ROWS; row++){
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+
+                    1.0f));
+            table.addView(tableRow);
+
+            for(int col = 0; col < NUM_COLS; col++){
+                Button button = new Button(this);
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                1.0f));
+                tableRow.addView(button);
+            }
+        }
+    }
+}
