c2dfc7a264f23e71ac4773fb29f65fb6517cf644
hqnguyen <hqnguyen@sfu.ca>
2020-02-03 20:40:11 -0800

    Add Gradle and MainMenuActivity layout

--- app/src/main/java/com/example/minesweeper/MainMenuActivity.java
+++ app/src/main/java/com/example/minesweeper/MainMenuActivity.java
@@ -2,7 +2,10 @@ package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        setupPlayGameButton();
+    }
+
+    private void setupPlayGameButton() {
+        Button playGameButton = (Button) findViewById(R.id.playGameButton);
+        playGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
