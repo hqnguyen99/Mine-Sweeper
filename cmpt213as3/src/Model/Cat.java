2959c69c6c71db302c9a5618d31650445376356f
devinlu <ludevinl@sfu.ca>
2020-02-05 00:09:03 -0800

    init push

--- cmpt213as3/src/Model/Cat.java
+++ cmpt213as3/src/Model/Cat.java
@@ -1,5 +1,29 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Cat {
-    private int direction;  // 0 1 2 3 -> N E S W
+    private int prevDir = -1;  // 0 1 2 3 -> N E S W
+    private List<Integer> lst = new ArrayList<>();
+
+    public Cat() {
+        for (int i = 0; i < 4; i++) {
+            lst.add(i);
+        }
+    }
+
+    public void move() {
+        Collections.shuffle(lst);
+
+        if (prevDir == -1) {
+
+        } else {
+
+        }
+
+    }
+
 }
--- cmpt213as3/src/Model/Cheese.java
+++ cmpt213as3/src/Model/Cheese.java
@@ -3,4 +3,11 @@ package Model;
 public class Cheese {
     private int amount;
 
+    public Cheese(int amount) {
+        this.amount = amount;
+    }
+
+    private void populateCheese() {
+
+    }
 }
