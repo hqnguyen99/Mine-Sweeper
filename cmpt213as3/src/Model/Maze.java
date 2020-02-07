d5b980e45a58516117cc3bfdb4b48028578c5845
devinlu <ludevinl@sfu.ca>
2020-02-06 19:06:56 -0800

    init push

--- cmpt213as3/src/Model/Maze.java
+++ cmpt213as3/src/Model/Maze.java
@@ -1,7 +1,7 @@
 package Model;
 
 public class Maze {
-    private static final int ROWS = 5;
-    private static final int COLS = 5;
+    private static final int ROWS = 20;
+    private static final int COLS = 15;
     private int[][] grid = new int[ROWS][COLS];
 }
